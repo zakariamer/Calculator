61d93d1f4eaf98edb37e9a6cfe446bbb47e1354d
Zakaria Merzougui <zakariamer123@gmail.com>
2025-02-11 23:04:09 -0500

    completed step 3, now added division and multiplication

--- Calculator.java
+++ Calculator.java
@@ -8,13 +8,22 @@ public class Calculator {
         int num1 = scan.nextInt();
         int num2 = scan.nextInt();
 
-        System.out.println("You wanna add or subtract? (1 = add | 2 = subtract)");
+        System.out.println("You wanna add, subtract, multiple, or divide? (1 = add | 2 = subtract | 3 = multiply | 4 = divide)");
         int answer = scan.nextInt();
 
-        if (answer == 1){
-            System.out.println(num1 + num2);
-        } else {
-            System.out.println(num1 - num2);
+        switch (answer) {
+            case 1:
+                System.out.println(num1 + num2);
+                break;
+            case 2:
+                System.out.println(num1 - num2);
+                break;
+            case 3:
+                System.out.println(num1 * num2);
+                break;
+            case 4:
+                System.out.println(num1 / num2);
+                break;
         }
 
 
