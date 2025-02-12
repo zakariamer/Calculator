699fd7ecddf942d6535acbba0a94e24c40be7bff
Zakaria Merzougui <zakariamer123@gmail.com>
2025-02-11 22:56:53 -0500

    completed step 1

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args){
+        Scanner scan  = new Scanner(System.in);
+
+        System.out.println("Enter two whole numbers...");
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+
+        System.out.println(num1 + num2);
     }
 
 }
