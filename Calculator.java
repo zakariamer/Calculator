69716a1b9ac8bae0bdfa6a3096076b52b586f009
Zakaria Merzougui <zakariamer123@gmail.com>
2025-02-11 23:18:10 -0500

    I forgot to screenshot during the cycle process so I just added an extra print statement

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args){
+        System.out.println("Just added this because I forgot to screenshot when working on the assignment");
         Scanner scan  = new Scanner(System.in);
 
         System.out.println("Enter two whole numbers...");
